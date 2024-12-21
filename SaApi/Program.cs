ca8b273b60767d2874beae359fcf859d1972f714
Jimmy Tjotola <tjotolajimmy@gmail.com>
2024-12-20 17:05:28 -0800

    Adjustments for DI

--- SaApi/Program.cs
+++ SaApi/Program.cs
@@ -1,23 +1,30 @@
+using Gaming.Application.Interfaces;
+using Gaming.Application.Services;
+using Gaming.Domain.Interfaces;
+using Gaming.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<GameContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IGameRepository, GameRepository>();
+builder.Services.AddScoped<IGameService, GameService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
